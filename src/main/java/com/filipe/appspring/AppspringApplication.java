63e4c18539ca4a281ff6bc53cd153199c2a813d3
Filipe Nascimento <filipe.n.silva@hotmail.com>
2022-06-08 20:46:05 -0300

    Projeto criado

---
+++ src/main/java/com/filipe/appspring/AppspringApplication.java
@@ -0,0 +1,13 @@
+package com.filipe.appspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppspringApplication.class, args);
+	}
+
+}
