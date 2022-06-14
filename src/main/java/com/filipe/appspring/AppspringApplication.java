adcd7e150d722844e1d155ea844248cc4b0a5087
Filipe Nascimento <filipe.n.silva@hotmail.com>
2022-06-13 21:30:37 -0300

    Criando operacao de instanciacao de categorias no H2

--- src/main/java/com/filipe/appspring/AppspringApplication.java
+++ src/main/java/com/filipe/appspring/AppspringApplication.java
@@ -1,13 +1,32 @@
 package com.filipe.appspring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.filipe.appspring.domain.Categoria;
+import com.filipe.appspring.repositories.CategoriaRepository;
+import com.filipe.appspring.services.CategoriaService;
+
 @SpringBootApplication
-public class AppspringApplication {
+public class AppspringApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppspringApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
 }
