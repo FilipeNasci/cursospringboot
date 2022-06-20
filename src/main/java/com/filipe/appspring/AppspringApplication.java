dc6a15bbe26299af0e7be2d56e7a07ac7ca17af1
Filipe Nascimento <filipe.n.silva@hotmail.com>
2022-06-20 16:18:52 -0300

    Criacao de entidade Estado e Cidade e repositories

--- src/main/java/com/filipe/appspring/AppspringApplication.java
+++ src/main/java/com/filipe/appspring/AppspringApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.filipe.appspring.domain.Categoria;
+import com.filipe.appspring.domain.Cidade;
+import com.filipe.appspring.domain.Estado;
 import com.filipe.appspring.domain.Produto;
 import com.filipe.appspring.repositories.CategoriaRepository;
+import com.filipe.appspring.repositories.CidadeRepository;
+import com.filipe.appspring.repositories.EstadoRepository;
 import com.filipe.appspring.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class AppspringApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppspringApplication.class, args);
@@ -44,7 +54,18 @@ public class AppspringApplication implements CommandLineRunner {
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(prod1, prod2,prod3));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
 		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));		
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 	
 }
---
+++ src/main/java/com/filipe/appspring/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.filipe.appspring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.filipe.appspring.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
