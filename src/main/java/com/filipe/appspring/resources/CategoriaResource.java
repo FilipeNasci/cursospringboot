ba60716f947bd406f2446708cf7292156cfe9173
Filipe Nascimento <filipe.n.silva@hotmail.com>
2022-08-05 22:48:56 -0300

    list Categoria usando DTO

--- src/main/java/com/filipe/appspring/resources/CategoriaResource.java
+++ src/main/java/com/filipe/appspring/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.filipe.appspring.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.filipe.appspring.domain.Categoria;
+import com.filipe.appspring.dto.CategoriaDTO;
 import com.filipe.appspring.services.CategoriaService;
 
 @RestController
@@ -49,5 +52,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping()
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> categorias = service.findAll();
+		List<CategoriaDTO> listDto = categorias.stream().map(categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDto);
 	}
 }
