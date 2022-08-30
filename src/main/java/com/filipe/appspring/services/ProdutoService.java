f4a6bb3149e3d078ceae08dc8891ed6d1d6c9fc3
Filipe Nascimento <filipe.n.silva@hotmail.com>
2022-08-29 22:22:41 -0300

    Busca de Produto por nome e categoria

---
+++ src/main/java/com/filipe/appspring/services/ProdutoService.java
@@ -0,0 +1,38 @@
+package com.filipe.appspring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.filipe.appspring.domain.Categoria;
+import com.filipe.appspring.domain.Produto;
+import com.filipe.appspring.repositories.CategoriaRepository;
+import com.filipe.appspring.repositories.ProdutoRepository;
+import com.filipe.appspring.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto buscar(Integer id) {
+		Optional<Produto> categoria = repo.findById(id);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.search(nome, categorias, pageRequest);
+		
+	}
+}
