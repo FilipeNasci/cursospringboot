4189006123874bf73cd0e50c66c3dbbea79de100
Filipe Nascimento <filipe.n.silva@hotmail.com>
2022-10-18 22:25:06 -0300

    restricao conteudo cliente recupera seus pedidos

--- src/main/java/com/filipe/appspring/services/PedidoService.java
+++ src/main/java/com/filipe/appspring/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.filipe.appspring.domain.Cliente;
 import com.filipe.appspring.domain.ItemPedido;
 import com.filipe.appspring.domain.PagamentoComBoleto;
 import com.filipe.appspring.domain.Pedido;
@@ -14,6 +18,8 @@ import com.filipe.appspring.domain.enums.EstadoPagamento;
 import com.filipe.appspring.repositories.ItemPedidoRepository;
 import com.filipe.appspring.repositories.PagamentoRepository;
 import com.filipe.appspring.repositories.PedidoRepository;
+import com.filipe.appspring.security.UserSS;
+import com.filipe.appspring.services.exceptions.AuthorizationException;
 import com.filipe.appspring.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -68,5 +74,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(pedido.getItens());
 		emailService.sendOrderConfirmation(pedido);
 		return pedido;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.findById(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
